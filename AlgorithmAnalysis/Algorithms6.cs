e033106f97560fa371af0eb2e12e980760fb0a57
Tylor Trub <tylortrub@gmail.com>
2024-08-18 15:00:37 -0400

    Added Algorithms6 class. Added problem description.

---
+++ AlgorithmAnalysis/Algorithms6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /* Problem: Create a function that returns search results back, focusing on performance.
+         *  Example input parameter:
+         *      1232,           1038,          4498,
+         *      298,            8786,          1036,
+         *      3132,           582,           7632,  
+         *      4174,           1103,          5394
+         *  Other notes:
+         *      The table does not have a guaranteed order or data values
+         *      The order of priorities for the solution should be: Performance, Elegance/Style, and Correctness.
+        */
+    public class Algorithms6
+    {
+
+    }
+}
